778e5afd55eacb5858d5b3a9bde92c32e3d4980e
Murilo <muriloflimaa@gmail.com>
2022-06-07 15:43:38 -0300

    tst-10

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="pt-BR">
-            <Head />
+            <Head>
+                <title>BuyPhone</title>
+                <meta name="description" content="Buy Phone"></meta>
+
+                {/* <link
+                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
+                    rel="stylesheet"
+                ></link> */}
+
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
+                    rel="stylesheet"
+                />
+
+                <link rel="icon" type="image/ico" href="/favicon.ico" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
