6b7e3b6aa31052c66843548c4858f8b1d116974b
Murilo <muriloflimaa@gmail.com>
2022-06-07 16:22:20 -0300

    tst-15

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,30 +1,55 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-    return (
-        <Html lang="pt-BR">
-            <Head>
-                <title>BuyPhone</title>
-                <meta name="description" content="Buy Phone"></meta>
-
-                {/* <link
-                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
-                    rel="stylesheet"
-                ></link> */}
-
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
-                    rel="stylesheet"
-                />
-
-                <link rel="icon" type="image/ico" href="/favicon.ico" />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    )
+export class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="pt-BR">
+                <Head>
+                    {/* favicon & other app icons */}
+                    <link rel="icon" href="/favicon.ico" />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="/site.webmanifest" />
+                    <link
+                        rel="mask-icon"
+                        href="/safari-pinned-tab.svg"
+                        color="#22d3ee"
+                    />
+                    <meta name="msapplication-TileColor" content="#22d3ee" />
+                    <meta name="theme-color" content="#22d3ee" />
+                    {/* Google Fonts */}
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
+                        rel="stylesheet"
+                    ></link>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
+
+export default MyDocument
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
-import { Html } from 'next/document'
 
 function Home() {
     return (
         <>
             <Head>
                 <title>BuyPhone</title>
+
                 <meta name="description" content="Buy Phone"></meta>
 
                 {/* <link
@@ -14,14 +14,14 @@ function Home() {
                     rel="stylesheet"
                 ></link> */}
 
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&amp;display=swap"
                     rel="stylesheet"
                 />
 
-                <link rel="icon" type="image/ico" href="/favicon.ico" />
+                <link rel="icon" type="image/ico" href="/favicon.ico" /> */}
             </Head>
             <main></main>
         </>
