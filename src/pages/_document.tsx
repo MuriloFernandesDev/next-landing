e73760c57f555f4f879344dcf9c9b36012f81b0e
Murilo <muriloflimaa@gmail.com>
2022-06-08 13:54:18 -0300

    finalizacao 00

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,30 +1,6 @@
-import Document, {
-    Html,
-    Head,
-    Main,
-    NextScript,
-    DocumentContext,
-} from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
-        const originalRenderPage = ctx.renderPage
-
-        // Run the React rendering logic synchronously
-        ctx.renderPage = () =>
-            originalRenderPage({
-                // Useful for wrapping the whole react tree
-                enhanceApp: (App) => App,
-                // Useful for wrapping in a per-page basis
-                enhanceComponent: (Component: any) => Component,
-            })
-
-        // Run the parent `getInitialProps`, it now includes the custom `renderPage`
-        const initialProps = await Document.getInitialProps(ctx)
-
-        return initialProps
-    }
-
+export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="pt-BR">
@@ -56,6 +32,7 @@ class MyDocument extends Document {
                     />
                     <meta name="msapplication-TileColor" content="#22d3ee" />
                     <meta name="theme-color" content="#22d3ee" />
+
                     {/* Google Fonts */}
                     <link
                         rel="preconnect"
@@ -75,5 +52,3 @@ class MyDocument extends Document {
         )
     }
 }
-
-export default MyDocument
